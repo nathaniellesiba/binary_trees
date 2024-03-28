1273e74df97caa81d1fac69b80077c0c0f8d772a
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-03-28 23:06:33 +0200

    T37 try

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -7,17 +7,19 @@ typedef struct binary_tree_s {
 } heap_t;
 
 /**
-* heap_insert - Function to insert a value into the Max Binary Heap
+* heap_insert - Function to insert
+* a value into the Max Binary Heap
 * @root: pointer to the root node
 * @value: the value to be inserted
-* Return: pointer to the new node on success, or NULL on failure
+* Return: pntr new node on success
 */
+
 heap_t *heap_insert(heap_t **root, int value)
 {
     heap_t *new_node = (heap_t *)malloc(sizeof(heap_t));
-    if (!new_node) 
+    if (!new_node)
     {
-        return NULL;  // Return NULL on failure
+        return NULL;
     }
 
     new_node->n = value;
@@ -32,31 +34,13 @@ heap_t *heap_insert(heap_t **root, int value)
 
     if (value <= (*root)->n)
     {
-        new_node->left = *root;
-        *root = new_node;
+        new_node->left = (*root)->left;
+        (*root)->left = new_node;
     }
     else
     {
-        heap_t *current = *root;
-        while (current->left != NULL && current->right != NULL)
-        {
-            if (value <= current->left->n)
-            {
-                current = current->left;
-            }
-            else
-            {
-                current = current->right;
-            }
-        }
-        if (current->left == NULL)
-        {
-            current->left = new_node;
-        }
-        else
-        {
-            current->right = new_node;
-        }
+        new_node->left = (*root)->right;
+        (*root)->right = new_node;
     }
 
     return new_node;
