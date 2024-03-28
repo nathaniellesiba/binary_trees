c64aca6cffca7ccd54f19637a19dca0c722b547f
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-03-28 21:39:24 +0200

    BTreeFunctionAddT0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,17 +1,17 @@
 #include "binary_trees.h"
 
 /**
-*binary_tree_node - takes pointr to parent
-*@binary_tree_t - the tree node
-*
-*/
+ * binary_tree_node - Create a binary node
+ * @parent: node parent
+ * @value: Value of the node
+ * Return: the new node
+ */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	new_node = malloc(sizeof(binary_tree_t));
-	
+	new_node = malloc(sizeof(binary_tree_t));	
 	if (new_node == NULL)
 	{
 		return (NULL);
--- binary_trees.h
+++ binary_trees.h
@@ -76,7 +76,7 @@ heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
-
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 
 
