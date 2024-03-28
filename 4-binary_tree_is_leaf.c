12d8b259d32bc477151dfaea08521991df0c0cbf
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-03-28 23:17:39 +0200

    T4 brackets

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -9,15 +9,15 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 {
     if (node == NULL)
     {
-        return 0;
+        return (0);
     }
 
     if (node->left == NULL && node->right == NULL)
     {
-        return 1;  /* Return 1 if node has no children (i.e., it is a leaf)*/
+        return (1);  /* Return 1 if node has no children (i.e., it is a leaf)*/
     }
     else
     {
-        return 0;  /*Return 0 if node has at least one child*/
+        return (0);  /*Return 0 if node has at least one child*/
     }
 }
