c12443d4b2fef50bad2a5f90d5e45de4fbbebd7f
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-03-28 21:30:04 +0200

    parent

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,16 +10,19 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new_node;
+
+	new_node = malloc(sizeof(binary_tree_t));
+	
 	if (new_node == NULL)
 	{
-		return;
+		return (NULL);
 	}
 
-    new_node->value = value;
-    new_node->left = NULL;
-    new_node->right = NULL;
-    new_node->parent = parent;
+	new_node->value = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	I[Inew_node->parent = parent;
 
-    return new_node;
+    return (new_node);
 }
