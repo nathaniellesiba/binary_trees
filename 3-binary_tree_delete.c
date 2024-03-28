b7ab5f591cadd053b83a92400fed5ac0d7a46313
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-03-28 21:07:20 +0200

    test DLT T3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+*binary_tree_delete - takes pointer
+*to the root node of the tree
+*as a parameter
+*@binary_tree_t: the node
+*@*tree: the tree
+*Return: null
+*/
+
+void binary_tree_delete(binary_tree_t *tree) 
+{
+    if (tree == NULL)
+    {
+        return;
+    }
+    
+    binary_tree_delete(tree->left);
+    binary_tree_delete(tree->right);
+    
+    free(tree);
+}
