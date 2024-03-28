c907bd44c879be5cf3871da31c3322d9e8c6c49c
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-03-28 20:59:32 +0200

    T0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,23 +2,25 @@
 #include <stdio.h>
 #include "binary_trees.h"
 
-typedef struct binary_tree_s {
-    int value;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
-    struct binary_tree_s *parent;
-} binary_tree_t;
+/**
+*binary_tree_node - takes pointr to parent
+*@binary_tree_t - the tree node
+*
+*/
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) {
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
     binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL) {
-        return NULL; /*Return NULL on failure*/
+    
+    if (new_node == NULL)
+    {
+        return NULL;
     }
-
-    new_node->value = value;
+    
+    new_node->parent = parent;
+    new_node->n = value;
     new_node->left = NULL;
     new_node->right = NULL;
-    new_node->parent = parent;
-
-    return new_node; /*Return a pointer to the new node*/
+    
+    return new_node;
 }
